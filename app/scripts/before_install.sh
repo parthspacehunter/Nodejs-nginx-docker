c72c0f76da78a9f9aa29e597f745b617570c7948
Parth <Chinu@Vikrants-MacBook-Air.local>
2023-04-11 14:22:18 +1200

    added appspec file for code depoly pipeline and scripts for deploying the app

---
+++ app/scripts/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/backend-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
